03727b094ac22d64365f6341b8c77b9e436c5b0f
arnoldojqf <arnoldojqf@gmail.com>
2022-07-17 02:37:23 +0100

    Docker compose

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,25 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+  } = process.env;
+
+const mondodbUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+
+mongoose.connect(mondodbUri, connectionOptions).then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 mongoose.Promise = global.Promise;
 
 module.exports = {
