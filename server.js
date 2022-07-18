5357aa303d4bbb4c4a8d7f763aa233227e917246
arnoldojqf <arnoldojqf@gmail.com>
2022-07-18 19:59:46 +0100

    debug

--- server.js
+++ server.js
@@ -34,5 +34,6 @@ app.use(errorHandler);
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
+    console.log('Environment: ' + process.env.NODE_ENV);
     console.log('Server listening on port ' + port);
 });
